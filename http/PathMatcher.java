6d6c96d3376905bffa819b0deb5b8efc59e75fd7
dine <dine@20hz.co.uk>
2011-09-30 15:41:48 +0100

    ammedned PathMatcher
    To take normal path spec strings and alson include the path minus trailing / as a match if input path is preceeded by /*
    
    Also cached path Pattern matcher

--- http/PathMatcher.java
+++ http/PathMatcher.java
@@ -5,18 +5,25 @@ import java.util.regex.*;
 
 public class PathMatcher extends HttpHeaderMatcher {
 	String path=null;
+	private Pattern pMatcher=null;
 	
 	public PathMatcher(String p) {
 		path=p;
 	}
-	
+	private Pattern getMatcherPattern(HttpRequest request) throws IOException{
+		if(pMatcher!=null) return pMatcher;
+		String regex="";
+		if(path.endsWith("/*")){
+			regex=path.substring(0,path.length()-1);
+			regex+=".*";
+			regex+="|"+path.substring(0,path.length()-2);
+		}
+		return pMatcher=Pattern.compile(regex);
+
+	}
 	@Override
-	public boolean matchHeader(HttpRequest request) throws IOException{
-		String h=request.getHeader("get");
-		Pattern p = Pattern.compile(path);
-		Matcher m = p.matcher(h);
-		boolean b = m.matches();
-		return b;
+	public boolean matchHeader(HttpRequest request) throws IOException{		
+		return getMatcherPattern(request).matcher(request.getFullPath()).matches();
 	}
 	
 }
